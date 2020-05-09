0f62ae2f43345c9d7ff20723d4bdbc2e11ecd80e
yuya sato <ysato.5654@gmail.com>
2020-05-09 16:00:35 +0800

    add test case & support coverage

--- spec/histock/simplefilter_spec.rb
+++ spec/histock/simplefilter_spec.rb
@@ -1,9 +1,61 @@
 RSpec.describe Histock::Simplefilter do
-  it 'has a version number' do
-    expect(Histock::Simplefilter::VERSION).not_to be nil
-  end
+    it 'has a version number' do
+        expect(Histock::Simplefilter::VERSION).not_to be nil
+    end
 
-  it'does something useful' do
-    expect(true).to eq(true)
-  end
+    let :histock do
+        Histock::Simplefilter.new
+    end
+
+    let :code do
+        '2330'
+    end
+
+    describe '#monthly_revenue(code)' do
+        subject do
+            histock.monthly_revenue(code)
+        end
+
+        let :header do
+            ["年度/月份", "單月營收", "去年同月營收", "單月月增率", "單月年增率", "累計營收", "去年累計營收", "累積年增率"]
+        end
+
+        describe 'first index' do
+            it 'is correct' do
+                expect(subject.first).to eq(header)
+            end
+        end
+
+        describe 'other indexes' do
+            it 'are correct' do
+                subject.each do |e|
+                    expect(e.length).to eq(header.length)
+                end
+            end
+        end
+    end
+
+    describe '#dividend_policy(code)' do
+        subject do
+            histock.dividend_policy(code)
+        end
+
+        let :header do
+            ["所屬年度", "發放年度", "除權日", "除息日", "除權息前股價", "股票股利", "現金股利", "EPS", "配息率", "現金殖利率", "扣抵稅率", "增資配股率", "增資認購價"]
+        end
+
+        describe 'first index' do
+            it 'is correct' do
+                expect(subject.first).to eq(header)
+            end
+        end
+
+        describe 'other indexes' do
+            it 'are correct' do
+                subject.each do |e|
+                    expect(e.length).to eq(header.length)
+                end
+            end
+        end
+    end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,26 @@
+if ENV['CI']
+    require 'coveralls'
+    Coveralls.wear!
+    SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[Coveralls::SimpleCov::Formatter]
+    SimpleCov.start 'test_frameworks'
+end
+
+require 'simplecov'
+SimpleCov.start do
+    add_filter 'spec/histock/simplefilter_spec.rb'
+end
+
 require 'bundler/setup'
 require 'histock/simplefilter'
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = '.rspec_status'
+    # Enable flags like --only-failures and --next-failure
+    config.example_status_persistence_file_path = '.rspec_status'
 
-  # Disable RSpec exposing methods globally on `Module` and `main`
-  config.disable_monkey_patching!
+    # Disable RSpec exposing methods globally on `Module` and `main`
+    config.disable_monkey_patching!
 
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
+    config.expect_with :rspec do |c|
+        c.syntax = :expect
+    end
 end

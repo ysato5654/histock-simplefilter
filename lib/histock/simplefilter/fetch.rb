4967e382e0ff3a44dd4e56dd777ecebafd720e1c
yuya sato <ysato.5654@gmail.com>
2020-06-13 17:58:05 +0800

    update parse table

--- lib/histock/simplefilter/fetch.rb
+++ lib/histock/simplefilter/fetch.rb
@@ -27,9 +27,9 @@ module Histock
             when :profit_ratio, :income_rate
                 nodes = doc.xpath("//div[@class='row-stock w1060']/div[@class='tb-outline']/div/table[@class='tb-stock tbBasic']")
             when :price_to_earning_ratio
-                nodes = doc.xpath("//div[@class='row-stock w740']/table[@class='tb-stock tb-outline tbBasic']/tbody")
+                nodes = doc.xpath("//div[@class='row-stock w740']/table[@class='tb-stock tb-outline tbBasic']")
             when :price_book_ratio
-                nodes = doc.xpath("//div[@class='row-stock']/table[@class='tb-stock tb-outline tbBasic']/tbody")
+                nodes = doc.xpath("//div[@class='row-stock']/table[@class='tb-stock tb-outline tbBasic']")
             end
 
             raise InformationNotFound if nodes.empty?
@@ -42,23 +42,39 @@ module Histock
         end
 
         def parse_table query:, element:
+            if children_is_tbody(:element => element)
+                table = parse_tr(:query => query, :element => fetch_tbody(:element => element))
+            else
+                table = parse_tr(:query => query, :element => element)
+            end
+
             case query
             when :price_to_earning_ratio, :price_book_ratio
-                table = Array.new
+                _table = Array.new
 
-                _table = parse_tr(:query => query, :element => element)
+                _table.concat([table[0][0..1]])
 
-                table.concat([_table[0][0..1]])
-                _table.slice!(0)
+                # remove header
+                table.slice!(0)
 
-                until _table.flatten.empty? do
-                    table.concat(_table.map { |e| e.slice!(0, 2) })
+                until table.flatten.empty? do
+                    _table.concat(table.map { |e| e.slice!(0, 2) })
                 end
 
-                table
-            else
-                parse_tr(:query => query, :element => element)
+                table = _table
             end
+
+            table
+        end
+
+        def children_is_tbody element:
+            nodes = element.children
+
+            nodes.length.is_one? and nodes.first.name == 'tbody'
+        end
+
+        def fetch_tbody element:
+            element.children
         end
 
         # column
